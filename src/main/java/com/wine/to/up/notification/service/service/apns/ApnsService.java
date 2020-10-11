ee6f4e327be76043071eb61b2421d75c1de5c55c
Albert Shaidullin <32803823+3LITA@users.noreply.github.com>
2020-10-11 23:50:29 +0300

    Send APNS push notifications (#8)
    APNS pushes

---
+++ src/main/java/com/wine/to/up/notification/service/service/apns/ApnsService.java
@@ -0,0 +1,52 @@
+package com.wine.to.up.notification.service.service.apns;
+
+import com.eatthepath.pushy.apns.ApnsClient;
+import com.eatthepath.pushy.apns.ApnsClientBuilder;
+import com.eatthepath.pushy.apns.PushNotificationResponse;
+import com.eatthepath.pushy.apns.auth.ApnsSigningKey;
+import com.eatthepath.pushy.apns.util.SimpleApnsPushNotification;
+import com.wine.to.up.notification.service.domain.model.apns.ApnsPushNotificationRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
+
+@Service
+@Slf4j
+public class ApnsService {
+
+    private ApnsClient apnsClient;
+
+    public ApnsService(ApnsSettings settings) {
+        Path p = Paths.get(settings.getKeyFile());
+        try (InputStream stream = Files.newInputStream(p)) {
+            apnsClient = new ApnsClientBuilder()
+                    .setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
+                    .setSigningKey(ApnsSigningKey.loadFromInputStream(stream, settings.getTeamId(), settings.getKeyId()))
+                    .build();
+            log.info("Successfully initialized APNS");
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            log.error("Error initializing APNS client", e);
+        }
+    }
+
+    public PushNotificationResponse<SimpleApnsPushNotification> sendMessage(ApnsPushNotificationRequest request) {
+        SimpleApnsPushNotification notification = new SimpleApnsPushNotification(
+                request.getDeviceToken(), request.getTopic(), request.getPayload()
+        );
+        try {
+            return apnsClient.sendNotification(notification).get();
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("Failed to send notification to device " + request.getDeviceToken(), e);
+            return null;
+        }
+    }
+
+}
