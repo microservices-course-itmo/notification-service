a2d9705e4ffea0be48d162f2564eae6a6046d0c6
Albert <3litac@GMAIL.COM>
2020-10-11 21:56:44 +0400

    use pushy to send apns notifications

---
+++ src/main/java/com/wine/to/up/notification/service/service/apns/ApnsPushNotificationRequest.java
@@ -0,0 +1,16 @@
+package com.wine.to.up.notification.service.service.apns;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApnsPushNotificationRequest {
+    private String deviceToken;
+    private String topic;
+    private String payload;
+}
---
+++ src/main/java/com/wine/to/up/notification/service/service/apns/ApnsSettings.java
@@ -0,0 +1,16 @@
+package com.wine.to.up.notification.service.service.apns;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@ConfigurationProperties(prefix = "apns")
+@Component
+@Getter
+public class ApnsSettings {
+    private String serviceAccountFile;
+    private String teamId;
+    private String keyId;
+}
