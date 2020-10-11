63b83edff8b71d4b94b2e573c461f40141c9f62e
heisen1berg <rybtsov_i_v@mail.ru>
2020-10-11 13:57:43 +0300

    Firebase

--- src/main/java/com/wine/to/up/notification/service/domain/model/fcm/FcmPushNotificationRequest.java
+++ src/main/java/com/wine/to/up/notification/service/domain/model/fcm/FcmPushNotificationRequest.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class FcmPushNotificationRequest {
     private String title;
+    private String topic;
     private String message;
     private String clientToken;
 }
--- src/main/java/com/wine/to/up/notification/service/service/fcm/FcmService.java
+++ src/main/java/com/wine/to/up/notification/service/service/fcm/FcmService.java
@@ -1,32 +1,61 @@
 package com.wine.to.up.notification.service.service.fcm;
 
-import java.time.Duration;
-import com.google.firebase.messaging.AndroidConfig;
-import com.google.firebase.messaging.AndroidNotification;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import com.wine.to.up.notification.service.domain.model.fcm.FcmPushNotificationRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @Service
 public class FcmService {
-    
+
 
     private Message.Builder getMessageBuilder(FcmPushNotificationRequest request) {
         AndroidConfig config = getAndroidConfig();
         return Message.builder()
-            .setAndroidConfig(config)
-            .setNotification(new Notification(request.getTitle(), request.getMessage()));
+                .setAndroidConfig(config)
+                .setNotification(new Notification(request.getTitle(), request.getMessage()));
     }
 
     private AndroidConfig getAndroidConfig() {
         return AndroidConfig.builder()
-            .setTtl(Duration.ofMinutes(2).toMillis())
-            .setPriority(AndroidConfig.Priority.NORMAL)
-            .setNotification(AndroidNotification.builder()
-                .build()
-            )
-            .build();
+                .setTtl(Duration.ofMinutes(2).toMillis())
+                .setPriority(AndroidConfig.Priority.NORMAL)
+                .setNotification(AndroidNotification.builder()
+                        .build()
+                )
+                .build();
+    }
+
+    private ApnsConfig getApnsConfig(String topic) {
+        return ApnsConfig.builder()
+                .setAps(Aps.builder().setCategory(topic).setThreadId(topic).build()).build();
+    }
+
+    public void sendMessageWithoutData(FcmPushNotificationRequest request)
+            throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageWithoutData(request);
+        String response = sendAndGetResponse(message);
+        log.info("Sent message without data. Topic: " + request.getTopic() + ", " + response);
+    }
+
+    private String sendAndGetResponse(Message message) throws InterruptedException, ExecutionException {
+        return FirebaseMessaging.getInstance().sendAsync(message).get();
+    }
+
+    private Message getPreconfiguredMessageWithoutData(FcmPushNotificationRequest request) {
+        return getPreconfiguredMessageBuilder(request).setTopic(request.getTopic())
+                .build();
+    }
+
+    private Message.Builder getPreconfiguredMessageBuilder(FcmPushNotificationRequest request) {
+        AndroidConfig androidConfig = getAndroidConfig();
+        ApnsConfig apnsConfig = getApnsConfig(request.getTopic());
+        return Message.builder()
+                .setApnsConfig(apnsConfig).setAndroidConfig(androidConfig).setNotification(
+                        new Notification(request.getTitle(), request.getMessage()));
     }
 }
