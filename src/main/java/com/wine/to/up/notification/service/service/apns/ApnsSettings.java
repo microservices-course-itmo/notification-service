c5674da62bd93a27cdd2f33f9d606b59fdc84f99
Albert <3litac@GMAIL.COM>
2020-10-17 18:16:32 +0400

    Add @setter decorator for ApnsSettings

--- src/main/java/com/wine/to/up/notification/service/service/apns/ApnsSettings.java
+++ src/main/java/com/wine/to/up/notification/service/service/apns/ApnsSettings.java
@@ -2,6 +2,7 @@ package com.wine.to.up.notification.service.service.apns;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "apns")
 @Component
 @Getter
+@Setter
 public class ApnsSettings {
     private String keyFile;
     private String teamId;
