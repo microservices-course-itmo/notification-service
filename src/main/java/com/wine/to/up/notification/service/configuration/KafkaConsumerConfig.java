f16089d5b90d44f3f29d2676ed1ebb0892ad95a9
TNX2021 <tvdnx2021@gmail.com>
2020-10-31 12:44:56 +0300

    Task/UserServiceListener [3]

--- src/main/java/com/wine/to/up/notification/service/configuration/KafkaConsumerConfig.java
+++ src/main/java/com/wine/to/up/notification/service/configuration/KafkaConsumerConfig.java
@@ -1,13 +1,7 @@
-package com.wine.to.up.notification.service.messaging;
+package com.wine.to.up.notification.service.configuration;
 
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.LongDeserializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import com.wine.to.up.notification.service.domain.model.kafka.CatalogMessage;
+import com.wine.to.up.user.service.api.dto.WineResponse;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -18,8 +12,8 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.BatchMessagingMessageConverter;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,7 +29,7 @@ public class KafkaConsumerConfig {
 
     @Bean
     public KafkaListenerContainerFactory<?> batchFactory() {
-        ConcurrentKafkaListenerContainerFactory<Long, AbstractDto> factory =
+        ConcurrentKafkaListenerContainerFactory<Long, WineResponse> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.setBatchListener(true);
@@ -45,7 +39,7 @@ public class KafkaConsumerConfig {
 
     @Bean
     public KafkaListenerContainerFactory<?> singleFactory() {
-        ConcurrentKafkaListenerContainerFactory<Long, AbstractDto> factory =
+        ConcurrentKafkaListenerContainerFactory<Long, WineResponse> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.setBatchListener(false);
@@ -54,7 +48,7 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
-    public ConsumerFactory<Long, AbstractDto> consumerFactory() {
+    public ConsumerFactory<Long, WineResponse> consumerFactory() {
         return new DefaultKafkaConsumerFactory<>(consumerConfigs());
     }
 
@@ -77,51 +71,5 @@ public class KafkaConsumerConfig {
     @Bean
     public StringJsonMessageConverter converter() {
         return new StringJsonMessageConverter();
-    }
-
-    @Bean
-    public ConsumerFactory<Long, AbstractDto> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaGroupId);
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-        return props;
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<?> singleFactory() {
-        ConcurrentKafkaListenerContainerFactory<Long, AbstractDto> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        factory.setBatchListener(false);
-        factory.setMessageConverter(new StringJsonMessageConverter());
-        return factory;
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<?> batchFactory() {
-        ConcurrentKafkaListenerContainerFactory<Long, AbstractDto> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        factory.setBatchListener(true);
-        factory.setMessageConverter(new BatchMessagingMessageConverter(converter()));
-        return factory;
-    }
-
-    @Bean
-    public StringJsonMessageConverter converter() {
-        return new StringJsonMessageConverter();
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory() {
-        return new ConcurrentKafkaListenerContainerFactory<>();
     }
 }
--- src/main/java/com/wine/to/up/notification/service/messaging/UserServiceKafkaMessageHandler.java
+++ src/main/java/com/wine/to/up/notification/service/messaging/UserServiceKafkaMessageHandler.java
@@ -1,2 +1,64 @@
-package com.wine.to.up.notification.service.messaging;public class UserServiceKafkaMessageHandler {
+package com.wine.to.up.notification.service.messaging;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wine.to.up.commonlib.messaging.KafkaMessageHandler;
+import com.wine.to.up.notification.service.api.message.KafkaMessageSentEventOuterClass.KafkaMessageSentEvent;
+import com.wine.to.up.notification.service.domain.model.apns.ApnsPushNotificationRequest;
+import com.wine.to.up.notification.service.domain.model.kafka.CatalogMessage;
+import com.wine.to.up.notification.service.domain.model.fcm.FcmPushNotificationRequest;
+import com.wine.to.up.notification.service.mobile.NotificationSender;
+import com.wine.to.up.notification.service.mobile.apns.ApnsService;
+import com.wine.to.up.notification.service.mobile.apns.ApnsSettings;
+import com.wine.to.up.notification.service.mobile.fcm.FcmService;
+import com.wine.to.up.user.service.api.dto.UserTokens;
+import com.wine.to.up.user.service.api.dto.WineResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Component
+@Slf4j
+public class UserServiceKafkaMessageHandler implements KafkaMessageHandler<KafkaMessageSentEvent> {
+
+    private NotificationSender notificationSender;
+
+    @Autowired
+    public UserServiceKafkaMessageHandler(NotificationSender notificationSender) {
+        this.notificationSender = notificationSender;
+    }
+
+    @Override
+    @KafkaListener(id = "user-service-topic-listener", topics = {"wine-response-topic"}, containerFactory = "batchFactory")
+    public void handle(KafkaMessageSentEvent message) {
+        final WineResponse wineResponse;
+        try {
+            wineResponse = new ObjectMapper().readValue(message.getMessage(), WineResponse.class);
+            log.info("Message received:{}", wineResponse);
+            for (UserTokens userTokens : wineResponse.getUserTokens()) {
+                ApnsService apnsService = new ApnsService(new ApnsSettings());
+                FcmService fcmService = new FcmService();
+                if (!userTokens.getIosTokens().isEmpty()) {
+                    for (String token : userTokens.getIosTokens()) {
+                        apnsService.sendMessage(new ApnsPushNotificationRequest(token, "default", "payload"));
+                    }
+                }
+
+                if (!userTokens.getFcmTokens().isEmpty()) {
+                    for (String token : userTokens.getFcmTokens()) {
+                        fcmService.sendMessage(new FcmPushNotificationRequest("title", "message", token));
+                    }
+                }
+            }
+        } catch (JsonProcessingException e) {
+            log.error("Could not parse Kafka message from User Service", e);
+        } catch (InterruptedException | ExecutionException ex) {
+            log.error("Failed to send notification", ex);
+        }
+
+    }
 }
