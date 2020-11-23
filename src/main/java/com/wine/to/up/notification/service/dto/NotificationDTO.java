40ab3648f76d05dd925877f71dbbaaecba6c8fb2
Albert <3litac@GMAIL.COM>
2020-11-24 01:36:47 +0300

    Add wineId and dateTime

--- src/main/java/com/wine/to/up/notification/service/dto/NotificationDTO.java
+++ src/main/java/com/wine/to/up/notification/service/dto/NotificationDTO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -12,5 +14,7 @@ public class NotificationDTO {
     private long id;
     private String message;
     private NotificationType type;
+    private LocalDateTime dateTime;
     private long userId;
+    private long wineId;
 }
