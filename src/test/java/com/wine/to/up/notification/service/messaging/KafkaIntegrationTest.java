75992c264b8459d51b0a28be6ea2d1321adc7b5b
Viktor Kniazev <theonetwelfth@gmail.com>
2020-11-27 19:21:52 +0300

    ✨ Add hacky MockBean + change kafka broker ports

--- src/test/java/com/wine/to/up/notification/service/messaging/KafkaIntegrationTest.java
+++ src/test/java/com/wine/to/up/notification/service/messaging/KafkaIntegrationTest.java
@@ -1,12 +1,14 @@
 package com.wine.to.up.notification.service.messaging;
 
 import com.wine.to.up.notification.service.ServiceApplication;
+import com.wine.to.up.notification.service.components.NotificationServiceMetricsCollector;
 import com.wine.to.up.user.service.api.dto.WinePriceUpdatedResponse;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -21,21 +23,29 @@ public class KafkaIntegrationTest {
     @Autowired
     private KafkaTemplate<Long, WinePriceUpdatedResponse> kafkaTemplate;
 
-    private boolean testPassed=false;
+    @MockBean
+    private NotificationServiceMetricsCollector notificationServiceMetricsCollector;
+
+    private boolean testPassed = false;
 
     @Test
     public void test() {
-        final WinePriceUpdatedResponse testWine=new WinePriceUpdatedResponse();
+        final WinePriceUpdatedResponse testWine = new WinePriceUpdatedResponse();
         testWine.setWineName("integration test");
 
-        kafkaTemplate.send("wine-response-topic",testWine);
+        kafkaTemplate.send("wine-response-topic", testWine);
 
-        try {Thread.sleep(3000);} catch (InterruptedException ignore){}
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ignore) {
+        }
         Assert.assertTrue(testPassed);
     }
 
-    @KafkaListener(id = "test-user-service-topic-listener", topics = {"wine-response-topic"}, containerFactory = "singleFactory")
-    public void consumer(WinePriceUpdatedResponse winePriceUpdatedResponse){
-        if(winePriceUpdatedResponse.getWineName().equals("integration test"))testPassed=true;
+    @KafkaListener(id = "test-user-service-topic-listener", topics = {"wine-response-topic"},
+            containerFactory = "singleFactory")
+    public void consumer(WinePriceUpdatedResponse winePriceUpdatedResponse) {
+        if (winePriceUpdatedResponse.getWineName().equals("integration test"))
+            testPassed = true;
     }
 }
