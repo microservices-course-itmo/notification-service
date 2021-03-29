0e5c17545a426e930309230d2dba475212a2f841
darkace <dan270799@gmail.com>
2021-03-29 18:22:40 +0300

    expo notification added wine id to data

--- src/main/java/com/wine/to/up/notification/service/domain/model/expo/ExpoNotificationRequest.java
+++ src/main/java/com/wine/to/up/notification/service/domain/model/expo/ExpoNotificationRequest.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashMap;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,4 +15,5 @@ public class ExpoNotificationRequest {
     private String token;
     private String title;
     private String message;
+    private HashMap<String, Object> data;
 }
