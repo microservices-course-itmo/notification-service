b0e5091abbb15eb9007425a05d2efa2b1730320c
Albert <3litac@GMAIL.COM>
2021-03-31 10:46:08 +0300

    edit scripts

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 
 
 source ./.env
-docker-compose -f profiles/minimal-Infrastructure.yml up -d
-mvn spring-boot:run -s settings.xml
+docker-compose -f profiles/minimal-Infrastructure.yml up -d && mvn spring-boot:run -s settings.xml
